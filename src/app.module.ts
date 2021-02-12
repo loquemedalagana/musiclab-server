f5a2492387da16a9aeeb4ff0cf1bfd47bdbd89d9
loquemedalagana <nadamasquetu012@gmail.com>
2021-02-12 16:21:55 +0900

    edit package.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import * as Joi from 'joi';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { YoutubeChannelsModule } from './youtube-channels/youtube-channels.module';
@@ -6,7 +9,26 @@ import { YoutubeVideosModule } from './youtube-videos/youtube-videos.module';
 import { TagsModule } from './tags/tags.module';
 
 @Module({
-  imports: [YoutubeChannelsModule, YoutubeVideosModule, TagsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: process.env.NODE_ENV !== 'prod',
+      logging:
+        process.env.NODE_ENV !== 'prod' && process.env.NODE_ENV !== 'test',
+      entities: [],
+    }),
+    YoutubeChannelsModule,
+    YoutubeVideosModule,
+    TagsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
