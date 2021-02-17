5c327dc2d77cae858fffb7b8e752f822ebcf90f1
loquemedalagana <nadamasquetu012@gmail.com>
2021-02-17 13:37:31 +0900

    add cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,19 @@ import { AppModule } from './app.module';
 async function musiclab() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  // app.enableCors();
+
+  if (process.env.NODE_ENV === 'production') {
+    app.enableCors({
+      origin: 'https://musicsseolprise.com',
+      credentials: true,
+    });
+  } else {
+    app.enableCors({
+      origin: 'http://localhost:3000',
+      credentials: true,
+    });
+  }
+
   await app.listen(5000);
 }
 musiclab();
